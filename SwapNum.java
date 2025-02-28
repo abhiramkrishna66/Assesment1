19ae6a727643bf80daeb04c44f1b8cc5b2193e34
abhiramkrishna66 <147504110+abhiramkrishna66@users.noreply.github.com>
2025-02-28 15:56:38 +0530

    Create SwapNum.java

---
+++ SwapNum.java
@@ -0,0 +1,19 @@
+public class SwapNum {
+    public static void main(String[] args){
+        int arr[][]={{5,4,7},{1,8,3},{9,6,2}};
+       for(int i=0;i<arr.length;i++){
+        for(int j=0;j<arr[0].length;j++){
+            if(i>j){
+                System.out.print(arr[j][i]+" ");
+            }
+            else{
+                System.out.print(arr[i][j]+" ");
+            }
+        }
+        System.out.println();
+        }
+       }
+
+        }
+
+    
